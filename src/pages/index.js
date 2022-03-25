2102d9bb7b01c12453a6a52a14144c04feeb469f
Mike Mitchell <alchemistikk@gmail.com>
2022-03-25 11:06:42 -0400

    Add all post content to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,46 @@
-// Step 1: Import React
 import * as React from 'react'
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
+import { postLink } from '../components/layout.module.css'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { StaticImage } from 'gatsby-plugin-image'
 
-// Step 2: Define your component
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
       <Layout pageTitle="Home">
+        {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.slug}`} className={postLink}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+          </article>
+        ))
+      }
       </Layout>
   )
 }
 
-// Step 3: Export your component
+export const query = graphql`
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        slug
+        body
+      }
+    }
+  }
+`
+
 export default IndexPage
