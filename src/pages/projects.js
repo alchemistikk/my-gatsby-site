607ea635d7fd4d96d6d3855a2270387f1f14cf48
Mike Mitchell <alchemistikk@gmail.com>
2022-03-28 09:54:25 -0400

    Add projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+      <Layout pageTitle="Projects">
+        <p>Hello. My name is Mike and this is my about page. This is all great stuff. Awesome.</p>
+        <p>Hello. My name is Mike and this is my about page. This is all great stuff. Awesome.</p>
+
+        <p>Hello. My name is Mike and this is my about page. This is all great stuff. Awesome.</p>
+
+        <p>Hello. My name is Mike and this is my about page. This is all great stuff. Awesome.</p>
+
+      </Layout>
+  )
+}
+
+export default AboutPage
