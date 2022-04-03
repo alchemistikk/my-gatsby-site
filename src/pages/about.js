962b137c422793c2dd7b81f00856d76dec3dd27d
Mike Mitchell <alchemistikk@gmail.com>
2022-04-03 15:23:37 -0400

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,29 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import pic from '../images/pic.png'
+import { aboutPage, aboutSection } from '../components/layout.module.css'
 
 const AboutPage = () => {
   return (
       <Layout pageTitle="About">
-        <img src={pic} alt="Mike smiles for the camera." width="200" height="200" />
-        <p>React Developer 🔗 | ASQ Certified Six Sigma Green Belt 🥋 | ASQ Certified Quality Process Analyst ⏱️ | Financial Services Consultant 🪙 | Poker Player ♠️</p>
+        <div className={aboutPage}>
+          <img src={pic} alt="Mike smiles for the camera." width="200" height="200" />
+          <div className={aboutSection}>
+            <h2>Current</h2>
+            <p>React Developer 🔗</p>
+            <p>ASQ Certified Six Sigma Green Belt 🥋</p>
+            <p>ASQ Certified Quality Process Analyst ⏱️</p>
+            <p>Financial Services Consultant 🪙</p>
+            <p>Poker Player ♠️</p>
+          </div>
+          <div className={aboutSection}>
+          <h2>Former</h2>
+          <p>Business student 💼</p>
+          <p>Personal Tutor 📋</p>
+          <p>Retail Manager 🛍️</p>
+          <p>Production Supervisor 🚗</p>
+          </div>
+        </div>
       </Layout>
   )
 }
